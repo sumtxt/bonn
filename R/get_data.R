2fe4a73122515266034175e04f13cfc1a76413ea
Simone Maxand <26380742+smaxand@users.noreply.github.com>
2023-03-01 10:01:00 +0100

    zeit option for get_data

--- R/get_data.R
+++ R/get_data.R
@@ -3,7 +3,8 @@
 #' \code{get_data} retrieves a data frame with all available values for a variable
 #'
 #' @param variable variable identifier as character string
-#' @param geography geography identifier as character string  
+#' @param geography geography identifier as character string 
+#' @param zeit time identifier as character string
 #'
 #' @details 
 #' To get a list of available geographies use \code{\link{get_geographies}}. To 
@@ -27,51 +28,56 @@
 #' 
 #' 
 #' @export
-get_data <- function(variable,geography){
-
-	if( length(variable)!=1 ) stop ("Can not retrieve data for more than one variable at a time.")
-	if( length(geography)!=1 ) stop ("Can not retrieve data for more than one geography at a time.")
-
-	url <- paste0(inkar_url, "/Table/GetDataTable")
-
-	# Zeitbezug
-	zeitbezug <- get_zeitbezuege(
-		raumbezug=geography,
-		variable=variable)
-
-	zeitbezug_ <- zeitbezug[,c("Zeit", "IndID", "ZeitID")]
-
-	zeitbezug <- zeitbezug[, c("Gruppe", "IndID", "ZeitID", "RaumID")]
-
-	zeitbezug <- rename_var(zeitbezug, 'Gruppe', 'group')
-	zeitbezug <- rename_var(zeitbezug, 'RaumID', 'level')
-	zeitbezug <- rename_var(zeitbezug, 'IndID', 'indicator')
-	zeitbezug <- rename_var(zeitbezug, 'ZeitID', 'time')			
-
-	i <- list(Gruppe=variable)
-
-	s <- list(level=geography)
-
-	param <- list(
-		IndicatorCollection = list(i),
-		TimeCollection = zeitbezug,
-		SpaceCollection = list(s),
-		pageorder = "1")
-
-	r <- POST(url, body=param, encode = "json")
-
-	jsondata <- fromJSON(content(r))[[1]]
-
-	jsondata <- merge(jsondata, zeitbezug_, 
-		by.x=c("ID", "ZeitID"), 
-		by.y=c("IndID", "ZeitID"), 
-		all.y=TRUE)
-
-	jsondata$ZeitID <- NULL
-	jsondata$ID <- NULL
-
-	return(jsondata)
-	}
+get_data <- function(variable,geography, zeit=NULL){
+  
+  if( length(variable)!=1 ) stop ("Can not retrieve data for more than one variable at a time.")
+  if( length(geography)!=1 ) stop ("Can not retrieve data for more than one geography at a time.")
+  
+  url <- paste0(inkar_url, "/Table/GetDataTable")
+  
+  # Zeitbezug
+  zeitbezug <- get_zeitbezuege(
+    raumbezug=geography,
+    variable=variable)
+  
+  
+  zeitbezug_ <- zeitbezug[,c("Zeit", "IndID", "ZeitID")]
+  if(is.null(zeit)==FALSE){
+    zeitbezug_ <- zeitbezug_[zeitbezug_[,"Zeit"] %in% zeit,]}
+  
+  zeitbezug <- zeitbezug[, c("Gruppe", "IndID", "ZeitID", "RaumID")]
+  
+  zeitbezug <- rename_var(zeitbezug, 'Gruppe', 'group')
+  zeitbezug <- rename_var(zeitbezug, 'RaumID', 'level')
+  zeitbezug <- rename_var(zeitbezug, 'IndID', 'indicator')
+  zeitbezug <- rename_var(zeitbezug, 'ZeitID', 'time')	
+  if(is.null(zeit)==FALSE){
+    zeitbezug <- zeitbezug[zeitbezug[,"time"] %in% zeit,]}
+  
+  i <- list(Gruppe=variable)
+  
+  s <- list(level=geography)
+  
+  param <- list(
+    IndicatorCollection = list(i),
+    TimeCollection = zeitbezug,
+    SpaceCollection = list(s),
+    pageorder = "1")
+  
+  r <- POST(url, body=param, encode = "json")
+  
+  jsondata <- fromJSON(content(r))[[1]]
+  
+  jsondata <- merge(jsondata, zeitbezug_, 
+                    by.x=c("ID", "ZeitID"), 
+                    by.y=c("IndID", "ZeitID"), 
+                    all.y=TRUE)
+  
+  jsondata$ZeitID <- NULL
+  jsondata$ID <- NULL
+  
+  return(jsondata)
+}
 
 
 
